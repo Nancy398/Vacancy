da94efee5e505210c1d2053c1ccaab8e3e181f9a
Nancy398 <nancy@moohousing.com>
2025-05-01 17:05:08 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
 import streamlit as st
-
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+import pandas as pd
+import plotly.express as px
+from google.oauth2.service_account import Credentials
+import gspread
