602e012e3fd594b4623c0e8efa4577f72433f969
Nancy398 <nancy@moohousing.com>
2025-05-07 16:42:30 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -323,10 +323,11 @@ with tab1:
     out_signing_by_type = df[df['Status'] == 'Out for Signing'].drop_duplicates(subset=['Property Name','Property']).groupby('Type').size().reset_index(name='Out for Signing Count')
     st.dataframe(out_signing_by_type)
     st.dataframe(vacant_by_type)
-    st.dataframe(total_by_type)
+    
 
 # 计算每种类型的总房间数量
     total_by_type = df.groupby('Type')['Property'].nunique().reset_index(name='Total Units')
+    st.dataframe(total_by_type)
 # 将按类型分组的空房间数量与总房间数量合并
     vacant_by_type = pd.merge(vacant_by_type,out_signing_by_type,total_by_type, on='Type')
     vacant_by_type['Vacancy Rate'] = ((vacant_by_type['Vacant Units'] / vacant_by_type['Total Units']) * 100).apply(lambda x: f"{round(x, 2)}%")
