e24c50f3b1cc4226b30de21e155cc90e6587408b
Nancy398 <nancy@moohousing.com>
2025-05-07 16:58:31 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -320,6 +320,9 @@ with tab1:
     vacant_unique = vacant_with_dates.drop_duplicates(subset=['Property Name', 'Property'])
     vacant_by_type = vacant_unique.groupby('Type').size().reset_index(name='Vacant Units')
     out_signing_by_type = df[df['Status'] == 'Out for Signing'].drop_duplicates(subset=['Property Name','Property']).groupby('Type').size().reset_index(name='Out for Signing Count')
+    st.dataframe(vacant_by_type)
+    st.dataframe(out_signing_by_type)
+  
     vacant_by_type = vacant_by_type + out_signing_by_type
 
 # 计算每种类型的总房间数量
