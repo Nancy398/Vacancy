31207528f4c51fc4bff624432a6aaf860eb004b9
Nancy398 <nancy@moohousing.com>
2025-05-07 17:52:30 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -340,7 +340,7 @@ with tab1:
         st.info("No vacant units at this time.")
     else:
         total_summary = pd.DataFrame({
-            '总空房间数量': ([vacant_units+Out_for_Signing_units]),
+            '总空房间数量': [vacant_units],
             'Out for Signing 数量':[Out_for_Signing_units],
             '空租率': [vacancy_rate] 
           })
